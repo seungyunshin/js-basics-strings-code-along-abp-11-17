62e9a09f1e6d8ef36383fcc7560d1a15b6224da5
Seung-Yun <sseungyun@gmail.com>
2017-11-11 06:28:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Your Face';
 
-const welcomeMessage = 'Welcome to Flatbook,' + currentUser + '!';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
