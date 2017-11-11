02796d4db82c01db36ddaf102709fb240c48fac4
Seung-Yun <sseungyun@gmail.com>
2017-11-11 06:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Your Face';
 
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
