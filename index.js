1fb075b849301fcd9080e10cd7f1d9a4d46c6d25
Seung-Yun <sseungyun@gmail.com>
2017-11-11 06:29:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 const currentUser = 'Your Face';
 
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + welcomeMessage + currentUser '!'
